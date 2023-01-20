43c4c1dcef3014d228134621d11c650361e0607f
Patryk Glinka <patrykglinka2@gmail.com>
2023-01-20 23:29:32 +0100

    Revert "Added cats images to left and right sidebar in LogedMainView and ChatView"
    This reverts commit 29ac8ddc53686b7b92e799dbfa4766715029b3f3.

--- src/views/LogedMainView.styles.ts
+++ src/views/LogedMainView.styles.ts
@@ -1,37 +1,16 @@
 import styled from 'styled-components';
 
-export const LeftSidebarImg = styled.img`
-  width: 25%;
-  /* left: -240px; */
-  height: 100vh;
-  display: inline-block;
-  position: absolute;
-  /* display: none; */
-`;
-
-export const RightSidebarImg = styled.img`
-  width: 25%;
-  /* right: -240px; */
-  right: 0;
-  height: 100vh;
-  display: inline-block;
-  position: absolute;
-  /* display: none; */
-`;
-
 export const OuterWrapper = styled.div`
   width: 100%;
   height: 100vh;
   background-color: ${({ theme }) => theme.colors.customBlack};
-  display: flex;
 `;
 
 export const MainWrapper = styled.div`
-  width: 50%;
+  width: 60%;
   margin: 0 auto;
   height: 100vh;
   background-color: ${({ theme }) => theme.colors.gray};
-  z-index: 9999;
 
   @media (max-width: 700px) {
     height: 90vh;
